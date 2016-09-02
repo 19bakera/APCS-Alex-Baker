55ee18ad663be946061b567546a13ff26734e167
blairbaker <blairbaker@10.1.33.34>
2016-09-02 14:02:41 -0500

    "I Got Git!"

---
+++ BakerGitHub/src/bakergithub/BakerGitHub.java
@@ -0,0 +1,17 @@
+
+package bakergithub;
+
+/**
+ *
+ * @author blairbaker
+ */
+public class BakerGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("I Got Git.");
+    }
+    
+}
